7bded254afa7e708f78c1ba54cb58653c14e447c
AD\NAli <nali@lmkr.com>
2026-02-22 05:01:35 +0500

    Initial Commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['react-native-worklets-core/plugin', { globals: ['__labelImage'] }],
+    'react-native-reanimated/plugin',
+  ],
 };
---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  assets: ['./src/assets/fonts/'],
+  ios: {},
+  android: {
+    packageName: 'com.rnfacialrecognition',
+  },
+  // Path to your fonts directory
+};
